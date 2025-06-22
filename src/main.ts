1337fd8df0e60a813d616d2b3273603fa1dbb56b
Seho Lee <seho0808@vt.edu>
2025-06-22 22:02:34 +0900

    chore: swagger enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Coffee Order API')
+    .setDescription('Coffee Order System API Documentation')
+    .setVersion('1.0')
+    .addTag('coffee-order')
+    .build();
+
+  const document = SwaggerModule.createDocument(app as any, config);
+  SwaggerModule.setup('api', app as any, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
